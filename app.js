46e92446f15fbddaf5773b431e7af17b364951e1
Vinicius Fantinatto <vifanti@gmail.com>
2019-08-13 14:24:32 -0300

    Troca SO

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://vifanti:280695@crud-nodejs-vf8hk.mongodb.net/capacitacao-api-unimetrocamp?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 const app = express()
  
 app.use('/', require('./routes'))
